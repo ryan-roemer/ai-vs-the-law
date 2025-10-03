45f161015eb31c2aede4481def0246d39f0ce10f
Ryan Roemer <ryan@loose-bits.com>
2025-10-03 12:41:38 -0700

    Parking lot the live code

--- examples/human/index.js
+++ examples/human/index.js
@@ -1,11 +1,11 @@
 import fs from "fs/promises";
 
 async function findLicense() {
-  const pkgPath = process.argv[2];
-  const { license } = JSON.parse(await fs.readFile(pkgPath, "utf8"));
-  const { licenses } = await fetch(
+  const packagePath = process.argv[2];
+  const license = JSON.parse(await fs.readFile(packagePath, "utf8"))?.license;
+  const licenses = await fetch(
     "https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json",
-  ).then((res) => res.json());
+  ).then((res) => res.json())?.licenses;
 
   const licenseName = licenses.find((l) => l.licenseId === license)?.name;
   if (licenseName) {
